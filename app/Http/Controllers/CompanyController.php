8835f4ea463057ec9c2ebf5d6d2faec18096d839
Elmer <elmervelazquez@gmail.com>
2026-03-09 11:03:39 -0400

    adding swagger

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,24 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
-use App\Http\Requests\StoreCompanyRequest;
-use App\Http\Requests\UpdateCompanyRequest;
+use OpenApi\Attributes as OA;
 
+
+
+
+#[OA\Schema(
+    schema: 'Company',
+    type: 'object',
+    required: ['id','name'],
+    properties: [
+        new OA\Property(property: 'id', type: 'integer', example: 1),
+        new OA\Property(property: 'name', type: 'string', example: 'Acme Inc.')
+    ]
+)]
 class CompanyController
 {
-    /**
-     * Display a listing of the resource.
-     */
+    #[OA\Get(path: '/api/data.json', operationId: 'getData')]
+    #[OA\Response(response: '200', description: 'The data')]
     public function index()
     {
         return Company::paginate(10)->toResourceCollection();
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Company $company)
-    {
-        return $company->toResource();
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
 
+
+#[OA\Info(title: 'My First API', version: '0.1')]
 abstract class Controller
 {
     //
